b7ab32cda345ca8a915658f0bb3ce82ca8bd0dc9
Sandro Stikić <sandro@stikic.com>
2024-07-14 01:04:25 +0800

    wip

--- build-macos.sh
+++ build-macos.sh
@@ -6,9 +6,8 @@ set -euxo pipefail
 path="$1"
 kind="$2"
 
-cd "$path/src/platform"
 cmake -S . -B build -G Ninja -D "CMAKE_BUILD_TYPE=$kind" -D BUILD_STATIC_LIB=True -Wno-dev
 cmake --build build
 
-find . -name '*.dylib'
-find . -name '*.a'
+mkdir -p dist
+find build \( -name '*.a' -o -name '*.so' -o -name '*.dylib' \) -print -exec cp {} dist \;
