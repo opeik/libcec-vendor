a4225585131b90784faf0e01189dda1e0d4c64a5
Sandro Stikić <sandro@stikic.com>
2024-07-14 00:34:30 +0800

    wip

--- build-macos.sh
+++ build-macos.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env nix
-#! nix shell nixpkgs/nixos-24.05#python311 nixpkgs/nixos-24.05#cmake nixpkgs/nixos-24.05#ninja nixpkgs/nixos-24.05#tree --command bash
+#! nix shell nixpkgs/nixos-24.05#cmake nixpkgs/nixos-24.05#ninja --command bash
 
 set -euxo pipefail
 
@@ -10,16 +10,7 @@ kind="$2"
     set -euxo pipefail
 
     cd "$path/src/platform"
-    cmake -S . -B build -G Ninja -D "CMAKE_BUILD_TYPE=$kind" -Wno-dev
-    cmake --build build
-    sudo cmake --install build
-)
-
-(
-    set -euxo pipefail
-
-    cd "$path"
-    cmake -S . -B build -G Ninja -D "CMAKE_BUILD_TYPE=$kind" -Wno-dev
+    cmake -S . -B build -G Ninja -D "CMAKE_BUILD_TYPE=$kind" -D BUILD_STATIC_LIB=True -Wno-dev
     cmake --build build
 )
 
